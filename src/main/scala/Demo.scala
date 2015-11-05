bf5ab49d3f803bb8146d14665c26a0c68e6778c3
Yixuan Qiu <yixuan.qiu@cos.name>
2015-11-04 20:07:27 -0500

    read data from file

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,5 +1,6 @@
 package statr.stat598bd
 
+import scala.io.Source
 import breeze.linalg._
 import breeze.numerics._
 import breeze.stats.distributions._
@@ -9,19 +10,38 @@ object Demo {
         return 1.0 / (exp(- x * b) + 1.0)
     }
 
+    private def read_data(f: String): (DenseMatrix[Double], DenseVector[Double]) = {
+        var source = Source.fromFile(f)
+        val lines = source.getLines()
+        val first = lines.take(1).toArray
+        val n = lines.length + 1
+        val p = first(0).split(' ').length - 1
+        source.close()
+
+        val x = DenseMatrix.zeros[Double](n, p)
+        val y = DenseVector.zeros[Double](n)
+
+        source = Source.fromFile(f)
+        var i = 0
+        for(line <- source.getLines()) {
+            val l = line.split(' ')
+            y(i) = l(0).toDouble
+            x(i, ::) := (new DenseVector(l.drop(1).map(x => x.toDouble))).t
+            i += 1
+        }
+        source.close()
+
+        return (x, y)
+    }
+
     def main(args: Array[String]) {
-        // Simulate data
-        val n = 100
-        val p = 3
-        val dist_norm = new Gaussian(0, 1)
-        val x = DenseMatrix.fill(n, p) { dist_norm.draw() }
-        val b = DenseVector.fill(p) { dist_norm.draw() }
-        val px = pi(x, b)
-        val y = px.map(x => if(new Bernoulli(x).draw()) 1.0 else 0.0)
+        val f = "other/data.txt"
+        val (x, y) = read_data(f)
 
         // Build model
         val mod = new Logistic(x, y)
         mod.run()
         println(mod.coef)
+        println("# of iterations: " + mod.niter)
     }
 }
