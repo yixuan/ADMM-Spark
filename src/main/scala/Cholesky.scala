6d2f9fa795ee0f505715f034ebb4273cdb4bb8e3
Yixuan Qiu <yixuan.qiu@cos.name>
2015-11-07 16:26:04 -0500

    overload constructor

--- src/main/scala/Cholesky.scala
+++ src/main/scala/Cholesky.scala
@@ -5,25 +5,32 @@ import breeze.numerics._
 import org.ejml.data._
 import org.ejml.factory._
 
-class Cholesky(val A: DenseMatrix[Double]) {
-    // DenseMatrix (breeze) is column-major, DenseMatrix64F (EJML) is row-major,
-    // but since x is assumed to be symmetric, the result would be correct
-    private val A_wrapper = DenseMatrix64F.wrap(A.rows, A.cols, A.data)
+class Cholesky(val n: Int) {
+    private val solver = LinearSolverFactory.chol(n)
 
-    private val solver = LinearSolverFactory.chol(A.cols)
-    solver.setA(A_wrapper)
+    def this(A: DenseMatrix[Double]) = {
+        this(A.cols)
+        compute(A)
+    }
+
+    def compute(A: DenseMatrix[Double]) {
+        // DenseMatrix (breeze) is column-major, DenseMatrix64F (EJML) is row-major,
+        // but since x is assumed to be symmetric, the result would still be correct
+        val A_wrapper = DenseMatrix64F.wrap(n, n, A.data)
+        solver.setA(A_wrapper)
+    }
 
     def solve(b: DenseVector[Double]): DenseVector[Double] = {
-        val b_wrapper = DenseMatrix64F.wrap(b.size, 1, b.data)
-        val res = DenseVector.zeros[Double](b.size)
-        val res_wrapper = DenseMatrix64F.wrap(res.size, 1, res.data)
+        val b_wrapper = DenseMatrix64F.wrap(n, 1, b.data)
+        val res = DenseVector.zeros[Double](n)
+        val res_wrapper = DenseMatrix64F.wrap(n, 1, res.data)
         solver.solve(b_wrapper, res_wrapper)
         return res
     }
 
     def solve(b: DenseVector[Double], x: DenseVector[Double]) {
-        val b_wrapper = DenseMatrix64F.wrap(b.size, 1, b.data)
-        val x_wrapper = DenseMatrix64F.wrap(x.size, 1, x.data)
+        val b_wrapper = DenseMatrix64F.wrap(n, 1, b.data)
+        val x_wrapper = DenseMatrix64F.wrap(n, 1, x.data)
         solver.solve(b_wrapper, x_wrapper)
     }
 }
