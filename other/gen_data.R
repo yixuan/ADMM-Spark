ff678c51b3ff4f29fafd8131f4dd5b3c0ddfdaba
Yixuan Qiu <yixuan.qiu@cos.name>
2015-11-20 13:16:42 -0500

    generate large data set

--- other/gen_data.R
+++ other/gen_data.R
@@ -19,3 +19,13 @@ m3 = glmnet(x, y, family = "binomial", standardize = FALSE, intercept = FALSE)
 coef(m3, 2 / n, exact = TRUE)
 
 write.table(data.frame(y = y, x = x), "data.txt", col.names = FALSE, row.names = FALSE, sep = " ")
+
+
+
+set.seed(123)
+n = 10000
+p = 200
+x = matrix(rnorm(n * p), n)
+b = rnorm(p)
+y = rbinom(n, 1, px(x, b))
+write.table(data.frame(y = y, x = x), "data_large.txt", col.names = FALSE, row.names = FALSE, sep = " ")
